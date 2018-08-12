e71ec82170fba113211bc819c2917d6f8aba1894
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-12 12:25:05 +0300

    Update er.js

--- er.js
+++ er.js
@@ -1,43 +1,46 @@
-const Eris = require("eris");
-var codes = new Eris(process.env.BOT_TOKEN);
-var codes_id = "476167313960206367";
-                    var i = "0";
-                    var x = "0";
-codes.on("voiceChannelJoin", (msg) => {
-    x++;
-    codes.editChannel(codes_id, { name : "United " + x + ""});
-});
-codes.on("voiceChannelLeave", (msg) => {
-    x--;
-    codes.editChannel(codes_id, { name : "United " + x + ""});
-});
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const queue = new Map();
 
-codes.on("messageCreate", (msg) => {
-    if(msg.author.id !== "470500980342128650") return codes.createMessage('__**This Command is only for the bot Owner**__');
-    if(msg.content === "uv") {
-        let users = msg.channel.guild.members.map(m => m.user.id);
-        let messages = [];
-        messages.push(users);
-        setTimeout(function(){
-        while (i <= messages[0].length - 1) {
-            check = msg.channel.guild.members.get(messages[0][i]);
-        if(!check.voiceState.channelID){
-                i++;
-        }else{
-                x++;
-                i++;
-        }
-}
-    console.log(x);
-    codes.createMessage(msg.channel.id, "Voice Online Members Now Are: **"+x+"** Members!");
-    codes.editChannel(codes_id, { name : "United "+x+""});
-    messages = [];
-}, 1);
+client.on('ready', () => {
+  console.log('---------------');
+  console.log(' Music Bot Is Online')
+  console.log('---------------')
+  client.user.setStatus("Online")
+    client.user.setActivity(" Type ehelp",{type: 'WATCHING'})
+	
+});
+	
+            var prefix = "e" ;
+	
+	
+	/*
+البكجآت
+npm install discord.js
+npm install ytdl-core
+npm install get-youtube-id
+npm install youtube-info
+npm install simple-youtube-api
+npm install queue
+*/
+client.on('message', msg => {
+
+    if (msg.content == 'eplay') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join();
+     }
     }
-});
+}
+})
 
 
-  
 
-
-codes.connect(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
+client.login(process.env.BOT_TOKEN);  //
