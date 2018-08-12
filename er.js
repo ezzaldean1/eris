b4619cccf71980e19cf2e582721a0f58c7703562
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-12 12:32:10 +0300

    Update er.js

--- er.js
+++ er.js
@@ -13,7 +13,7 @@ client.on('ready', () => {
   console.log(' Music Bot Is Online')
   console.log('---------------')
   client.user.setStatus("Online")
-    client.user.setActivity(" Type ehelp",{type: 'WATCHING'})
+    client.user.setActivity("احبك  ♪",{type: 'PLAYING'})
 	
 });
 	
